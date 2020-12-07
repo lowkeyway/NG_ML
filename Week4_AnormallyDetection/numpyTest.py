b65d0f46e4891f16d0aebb4283c5e440eca9d9a3
lowkeyway <lowkeyway@126.com>
2020-12-07 16:24:51 +0800

    Add more test

--- Week4_AnormallyDetection/numpyTest.py
+++ Week4_AnormallyDetection/numpyTest.py
@@ -6,17 +6,32 @@ import numpy as np
 
 
 def main_func(argv):
-    x = np.mgrid[0:3, 0:3]
-    print("x = \n", x)
+    # x = np.mgrid[0:3, 0:3]
+    # print("x = \n", x)
+    #
+    # y = np.dstack(x)
+    # print("y = \n", y)
+    #
+    # z = np.eye(3)
+    # zCov = np.cov(z)
+    # print("z = \n", z)
+    # print("zCov = \n", zCov)
 
-    y = np.dstack(x)
+    X = np.array([[1, 5, 6],
+                  [4, 3, 9],
+                  [4, 2, 9],
+                  [4, 7, 2]])
+    print("X = \n", X)
+
+    x = X[0:2]
+    y = X[2:4]
+
+    print("x = \n", x)
     print("y = \n", y)
 
-    z = np.eye(3)
-    zCov = np.cov(z)
-    print("z = \n", z)
-    print("zCov = \n", zCov)
+    print("np.cov(X) = \n", np.cov(X))
 
+    print("np.cov(x, y) = \n", np.cov(x, y))
 
 
 if __name__ == '__main__':
